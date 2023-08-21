cde069cb8ca58bd1649212ac2035e78d146bbbdf
Sergey Bevzenko <bevzenko.sergey@gmail.com>
2023-08-21 12:20:27 +0200

    new version config 9

--- keyboards/bastardkb/charybdis/4x6/keymaps/default/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/default/keymap.c
@@ -76,16 +76,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [LAYER_THIRD] = LAYOUT(
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-       KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS,
+       KC_MS_U, KC_MS_D, KC_MS_L, KC_MS_R,  KC_NO,   KC_NO,    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        KC_ACL0, KC_ACL1, KC_ACL2, KC_WH_U, KC_WH_D, KC_WH_L,    KC_LBRC,   KC_P7,   KC_P8,   KC_P9, KC_RBRC, XXXXXXX,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        KC_BTN5, KC_BTN4, KC_BTN3, KC_BTN2, KC_BTN1, KC_WH_R,    KC_LEFT,  KC_DOWN,  KC_UP,   KC_RGHT, KC_PMNS, KC_PEQL,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-      KC_LCTL, EC_NORM, EC_SWAP, QK_REBOOT, EE_CLR,  QK_BOOT,   KC_MS_U,  KC_MS_D,  KC_MS_L,   KC_MS_R, KC_PSLS, KC_PDOT,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_LCTL, EC_NORM, EC_SWAP, QK_REBOOT, EE_CLR,  QK_BOOT,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
                                   XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,
-                                           _______, _______,      KC_P0
+                                           XXXXXXX, XXXXXXX,    XXXXXXX
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 };

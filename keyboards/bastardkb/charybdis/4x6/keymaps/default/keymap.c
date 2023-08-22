94e88fb01ffb29866e756d25789a460a6bc4d667
Sergey Bevzenko <bevzenko.sergey@gmail.com>
2023-08-22 16:56:59 +0200

    new version config 13

--- keyboards/bastardkb/charybdis/4x6/keymaps/default/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/default/keymap.c
@@ -63,11 +63,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
         KC_F12,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_GRV, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, KC_BTN2,
+       KC_GRV, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, XXXXXXX, KC_BTN2,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        KC_TRNS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   KC_LEFT,  KC_DOWN,  KC_UP, KC_RGHT, KC_LALT, KC_BTN1,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_TRNS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_DEL,    KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_LSFT, DRGSCRL,
+       KC_TRNS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_DEL,    KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_TRNS, DRGSCRL,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
                                   _______, _______, _______,   _______, _______,
                                            _______, _______,    _______

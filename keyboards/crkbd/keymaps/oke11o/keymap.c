697a8824c2a49b389768d7f8a50039ddb3a4c7fe
Sergey Bevzenko <bevzenko.sergey@gmail.com>
2024-10-02 15:51:38 +0200

    New life 2.1

--- keyboards/crkbd/keymaps/oke11o/keymap.c
+++ keyboards/crkbd/keymaps/oke11o/keymap.c
@@ -33,11 +33,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [1] = LAYOUT_split_3x6_3(
   //╭──────────────────────────────────────────────────────╮                   ╭──────────────────────────────────────────────────────╮
-       KC_TAB, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LCBR, KC_RCBR, XXXXXXX,
+       KC_TAB, KC_EXLM, KC_AT,  KC_HASH, KC_DLR,  KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LCBR, KC_RCBR, XXXXXXX,
  //├──────────────────────────────────────────────────────┤                   ├──────────────────────────────────────────────────────┤
-      KC_LSFT, KC_LALT, KC_LEFT,  KC_UP,  KC_DOWN,  KC_RGHT,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_LALT, KC_MINS,
+      KC_LSFT, KC_LALT, KC_LEFT, KC_UP,  KC_DOWN, KC_RGHT,                      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_LALT, KC_MINS,
   //├──────────────────────────────────────────────────────┤                   ├──────────────────────────────────────────────────────┤
-      KC_LCTL, KC_A, KC_LPRN, KC_RPRN,  KC_LBRC,  KC_RBRC,                  KC_COLN, KC_PEQL, KC_PCMM,  KC_PDOT, KC_GRV, KC_PPLS,
+      KC_LCTL, KC_A,   KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC,                      KC_COLN, KC_PEQL, XXXXXXX,  XXXXXXX, KC_GRV, KC_PPLS,
   //╰──────────────────────────────────────────────────────┤                   ├──────────────────────────────────────────────────────╯
                                     KC_ESC, LT(4,KC_ENT),  KC_LGUI,      KC_SPC,  KC_LSFT, KC_DEL
                                    //╰───────────────────────────╯   ╰───────────────────────────╯
@@ -45,9 +45,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [2] = LAYOUT_split_3x6_3(
   //╭──────────────────────────────────────────────────────╮                   ╭──────────────────────────────────────────────────────╮
-     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_PSLS, KC_MINS, KC_PAST, KC_PSLS, XXXXXXX,XXXXXXX,
+     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_PPLS, KC_MINS, KC_PAST, KC_PSLS, XXXXXXX,XXXXXXX,
   //├──────────────────────────────────────────────────────┤                   ├──────────────────────────────────────────────────────┤
-     XXXXXXX, XXXXXXX, XXXXXXX,    KC_F, XXXXXXX, XXXXXXX,                      KC_6,KC_7,KC_8,KC_9,KC_0,KC_PEQL,
+     KC_LSFT, XXXXXXX, XXXXXXX,    KC_F, XXXXXXX, XXXXXXX,                      KC_6,KC_7,KC_8,KC_9,KC_0,KC_PEQL,
   //├──────────────────────────────────────────────────────┤                   ├──────────────────────────────────────────────────────┤
      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_1,KC_2,KC_3, KC_4,KC_5,KC_PDOT,
   //╰──────────────────────────────────────────────────────┤                   ├──────────────────────────────────────────────────────╯
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [3] = LAYOUT_split_3x6_3(
   //╭──────────────────────────────────────────────────────╮                   ╭──────────────────────────────────────────────────────╮
-      KC_TAB,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_PSLS, KC_MINS, KC_PAST, KC_PSLS, XXXXXXX,XXXXXXX,
+      KC_TAB,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_PPLS, KC_MINS, KC_PAST, KC_PSLS, XXXXXXX,XXXXXXX,
   //├──────────────────────────────────────────────────────┤                   ├──────────────────────────────────────────────────────┤
       KC_LSFT, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,                       KC_6,KC_7,KC_8,KC_9,KC_0,KC_PEQL,
   //├──────────────────────────────────────────────────────┤                   ├──────────────────────────────────────────────────────┤
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
    [4] = LAYOUT_split_3x6_3(
   //╭──────────────────────────────────────────────────────────────────────╮     ╭──────────────────────────────────────────────────────╮
-     XXXXXXX, XXXXXXX, XXXXXXX, OSM(MOD_HYPR), KC_ENT, XXXXXXX,                   KC_PSLS, KC_MINS, KC_PAST, KC_PSLS, XXXXXXX,XXXXXXX,
+     XXXXXXX, XXXXXXX, XXXXXXX, OSM(MOD_HYPR), KC_ENT, XXXXXXX,                   KC_PPLS, KC_MINS, KC_PAST, KC_PSLS, XXXXXXX,XXXXXXX,
   //├──────────────────────────────────────────────────────────────────────┤     ├──────────────────────────────────────────────────────┤
      KC_GRV,                           OSM(MOD_LGUI|MOD_LSFT),
      OSM(MOD_LCTL|MOD_LSFT|MOD_LGUI),  OSM(MOD_LCTL|MOD_LALT|MOD_LGUI),
